3d95418a7690b861251b81db44d92ebd5f2b29ca
Wanderpsc <wanderpsc@gmail.com>
2026-04-04 22:08:09 -0300

    Remover botão Ver Projetos sem função

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,6 @@ function Hero() {
         <p className="hero__saudacao">Olá, eu sou</p>
         <h1 className="hero__nome">Wander Pires<span className="hero__cursor">|</span></h1>
         <p className="hero__subtitulo">Educador &bull; Naturopata &bull; Desenvolvedor Web</p>
-        <a href="#projetos" className="hero__btn">Ver Projetos</a>
       </div>
     </section>
   );
